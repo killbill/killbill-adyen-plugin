011379d60c89bfc6a63d3431f1a8eb4cd24eeb39
Pierre-Alexandre Meyer <pierre@mouraf.org>
2016-04-27 11:33:15 -0700

    jaxws: pass the X-Request-Id through all the way to Adyen
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- src/main/java/org/killbill/billing/plugin/adyen/client/jaxws/HttpHeaderInterceptor.java
+++ src/main/java/org/killbill/billing/plugin/adyen/client/jaxws/HttpHeaderInterceptor.java
@@ -1,7 +1,8 @@
 /*
- * Copyright 2014 Groupon, Inc
+ * Copyright 2014-2016 Groupon, Inc
+ * Copyright 2014-2016 The Billing Project, LLC
  *
- * Groupon licenses this file to you under the Apache License, version 2.0
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
  * License.  You may obtain a copy of the License at:
  *
@@ -24,14 +25,18 @@ import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
+import org.slf4j.MDC;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.MoreObjects;
 import com.google.common.collect.ImmutableList;
 
 public class HttpHeaderInterceptor extends AbstractPhaseInterceptor<Message> {
 
     @VisibleForTesting
     static final String X_REQUEST_ID = "x-request-id";
+    // See KillbillMDCInsertingServletFilter
+    private static final String MDC_REQUEST_ID = "req.requestId";
 
     public HttpHeaderInterceptor() {
         super(Phase.POST_PROTOCOL);
@@ -39,9 +44,12 @@ public class HttpHeaderInterceptor extends AbstractPhaseInterceptor<Message> {
 
     @Override
     public void handleMessage(final Message message) throws Fault {
+        // We know Kill Bill passed the X-Request-Id from the user in the slf4j MDC. Maybe one day, there will be a real API for it?
+        final String xRequestId = MoreObjects.firstNonNull(MDC.get(MDC_REQUEST_ID), UUID.randomUUID().toString());
+
         final Map<String, List> headers = (Map<String, List>) message.get(Message.PROTOCOL_HEADERS);
         try {
-            headers.put(X_REQUEST_ID, ImmutableList.<String>of(UUID.randomUUID().toString()));
+            headers.put(X_REQUEST_ID, ImmutableList.<String>of(xRequestId));
         } catch (final Exception e) {
             throw new Fault(e);
         }
